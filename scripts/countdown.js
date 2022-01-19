e3fcbcba6460b437e78a14bbc62392d5d6982dc3
gonbanchero <gonzabanchero@gmail.com>
2022-01-19 16:23:45 -0300

    agregado countdown.js

---
+++ scripts/countdown.js
@@ -0,0 +1,42 @@
+const timeElements = document.querySelectorAll('.countdown span');
+const body = document.querySelector('#body-container');
+const countdown = document.querySelector('#countdown');
+
+let nextDate = Date.now() + 213124121; // en milisegundos
+
+const renderCountDown = () => {
+	let now = new Date().getTime();
+
+	let gap = nextDate - now;
+
+	let second = 1000; // 1s
+	let minute = second * 60;
+	let hour = minute * 60;
+	let day = hour * 24;
+
+	let days = Math.floor(gap / day);
+	let hours = Math.floor((gap % day) / hour);
+	let minutes = Math.floor((gap % hour) / minute);
+	let seconds = Math.floor((gap % minute) / second);
+
+	console.log(days, hours, minutes, seconds);
+
+	timeElements[0].innerText = days;
+	timeElements[1].innerText = hours;
+	timeElements[2].innerText = minutes;
+	timeElements[3].innerText = seconds;
+};
+
+setInterval(function () {
+	renderCountDown();
+}, 1000);
+
+var myScrollFunc = function () {
+	var y = window.scrollY;
+	console.log(y);
+	if (y >= 200) {
+		countdown.style.visibility = 'visible';
+	}
+};
+
+window.addEventListener('scroll', myScrollFunc);
